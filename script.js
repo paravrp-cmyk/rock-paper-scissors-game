e9236024d2e3ef39739a8a959064800d14907ab0
Maks_pvrp <285418221+paravrp-cmyk@users.noreply.github.com>
2026-05-31 21:24:34 +0200

    add restart button, some changes to code, now game dont work after scoring 5 points until you click restert button. Some visual changes, add emodji for every button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btn = document.querySelectorAll('.choise-btn');
+const rstart = document.querySelector('.restart')
+const res = document.querySelector('.result');
 
 btn.forEach((button)=>{
     button.addEventListener('click', () =>{
@@ -7,15 +9,15 @@ btn.forEach((button)=>{
     });
 });
 
-function getCompChoice(){
+function getCompChoice() {
     let i = Math.random()
     let v;
     if (i < 0.33333){
-     v = 'rock';
+     v = 'Rock';
     } else if (i>0.33333 && i < 0.66666){
-     v = 'paper';
+     v = 'Paper';
     } else {
-     v ='scissors';
+     v ='Scissors';
     }
     return v;
 }
@@ -24,42 +26,30 @@ let humScore = 0;
 let compScore = 0;
 
 function playRound(hCoice, compChoice){
-    const res = document.querySelector('.result');
-    if (hCoice===compChoice){
-        humScore+=1;
-        compScore+=1;
-        res.textContent = `Your choice: ${hCoice}, Computer Choice: ${compChoice}. Score: ${humScore}-${compScore}`;
-    } else if (hCoice === 'rock' && compChoice === 'paper' || 
-        hCoice === 'paper' && compChoice === 'scissors' ||
-        hCoice === 'scissors' && compChoice === 'rock'){
-        compScore+=1;
-        res.textContent = `Your choice: ${hCoice}, Computer Choice: ${compChoice}. Score: ${humScore}-${compScore}`;
-    } else if (hCoice === 'paper' && compChoice === 'rock' ||
-        hCoice === 'rock' && compChoice === 'scissors' ||
-        hCoice === 'scissors' && compChoice === 'paper'){
-        humScore+=1;
-        res.textContent = `Your choice: ${hCoice}, Computer Choice: ${compChoice}. Score: ${humScore}-${compScore}`;
-    }
-
-    if (humScore === 5 || compScore === 5){
+    if (humScore === 5 || compScore === 5) {
         res.textContent = `GAME OVER! Score: ${humScore}-${compScore}`
-        humScore = 0;
-        compScore = 0;
+        return;
     }
 
+    if (hCoice===compChoice) {
+        humScore+=1;
+        compScore+=1;
+        res.innerText = `Your choice: ${hCoice}\nComputer Choice: ${compChoice}\nScore: ${humScore}-${compScore}`;
+    } else if (hCoice === 'Rock' && compChoice === 'Paper' || 
+        hCoice === 'Paper' && compChoice === 'Scissors' ||
+        hCoice === 'Scissors' && compChoice === 'Rock') {
+        compScore+=1;
+        res.innerText = `Your choice: ${hCoice}\nComputer Choice: ${compChoice}\nScore: ${humScore}-${compScore}`;
+    } else if (hCoice === 'Paper' && compChoice === 'Rock' ||
+        hCoice === 'Rock' && compChoice === 'Scissors' ||
+        hCoice === 'Scissors' && compChoice === 'Paper') {
+        humScore+=1;
+        res.innerText = `Your choice: ${hCoice}\nComputer Choice: ${compChoice}\nScore: ${humScore}-${compScore}`;
+        }
 }
 
-//*function playGame(){
-   //* playRound(humChoice, getCompChoice());
-   /**  console.log('=== Round 2 ===');
-    playRound(getHumChoice(),getCompChoice());
-    console.log('=== Round 3 ===');
-    playRound(getHumChoice(),getCompChoice());
-    console.log('=== Round 4 ===');
-    playRound(getHumChoice(),getCompChoice());
-    console.log('=== Round 5 ===');
-    playRound(getHumChoice(),getCompChoice());
-
-}
-playGame()*/
-
+rstart.addEventListener('click', () => {
+    humScore = 0;
+    compScore = 0;
+    res.textContent = "New game started! Choose your symbol!"
+})
